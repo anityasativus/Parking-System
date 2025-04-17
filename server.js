fdf7d9d9d731bc9c2ebf8e384ee51a94c2f61ccf
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-04-17 18:14:16 +0600

    f

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = new SerialPort({ path: "COM5", baudRate: 9600 }); // Change COM3 accordingly
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+
+app.use(express.static("public"));
+
+parser.on("data", (data) => {
+  console.log("Arduino:", data);
+  io.emit("arduino-data", data); // send to client
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
